e465745ad3db3fedf9138ceb7b7a0167d7cdf68c
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2020-01-11 11:41:46 -0700

    setup complete, now need to connect to API and to DB

--- schema.sql
+++ schema.sql
@@ -3,11 +3,19 @@ CREATE DATABASE hackprac;
 \c hackprac
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS content;
 
 CREATE TABLE users(
     id              SERIAL PRIMARY KEY, 
     username        VARCHAR(50),
     password        VARCHAR(50)
+);
+
+CREATE TABLE content(
+    id              SERIAL PRIMARY KEY, 
+    username        INTEGER REFERENCES users(id),
+    videoURL        VARCHAR(200),
+    mp3             VARCHAR(200),
 );
 
 
