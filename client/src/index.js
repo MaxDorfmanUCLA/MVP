c554f04e525075f706af3435ad19ab17f03d2810
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2020-01-11 12:27:22 -0700

    signup page and login page render dynamically

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/form.js'
+import Form from './components/form.js';
+import List from './components/list.js';
+import Signup from './components/signup.js';
 
 class App extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            page: 'signup'
+        }
     }
     render() {
+        var pageComponent = <div></div>
+        if (this.state.page === 'signup') {
+            pageComponent =  <Signup />
+        } else {
+            pageComponent =  
+            <div>
+                <Form />
+                <List />
+            </div>
+        }
         return (
-            <Form />
+            pageComponent
         )
     }
 }
